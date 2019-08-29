265bbe4c137967334d96763e39d20735d576deef
mu <cmu@gekko.ai>
2019-08-29 10:46:09 +0800

    chapter2-1

---
+++ chapter/chapter2-1/chapter2-1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// @Time    : 2019-08-29 10:41
+// @Author  : mu
+// @File    : chapter2-1.go
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
