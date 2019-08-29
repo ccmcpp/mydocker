7245731935bce490fa309154b0a5511a4ba037b6
mu <cmu@gekko.ai>
2019-08-29 11:54:01 +0800

    chapter2-1

--- chapter/chapter2-1/main.go
+++ chapter/chapter2-1/main.go
@@ -14,7 +14,12 @@ import (
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER |
+			syscall.CLONE_NEWNET,
+	}
+	cmd.SysProcAttr.Credential = & syscall.Credential{
+		Uid: uint32(1), Gid:uint32(1),
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
